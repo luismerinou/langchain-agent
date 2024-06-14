506521ab1d41c20bd5d955e956ae311a1da5a5ee
Luis Merino Ulizarna <lmerinoulizarna@gmail.com>
2024-06-14 17:11:28 +0200

    feat: initial configuration

---
+++ src/test/java/com/ai/langchainllm/LangchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.langchainllm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangchainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
