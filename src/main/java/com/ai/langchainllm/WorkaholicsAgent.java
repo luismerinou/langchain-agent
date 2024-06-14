df27836ab7d2541fc0d41640ed7a34f6174d403d
Luis Merino Ulizarna <lmerinoulizarna@gmail.com>
2024-06-14 19:38:55 +0200

    feat: update summary

--- src/main/java/com/ai/langchainllm/WorkaholicsAgent.java
+++ src/main/java/com/ai/langchainllm/WorkaholicsAgent.java
@@ -4,12 +4,19 @@ import dev.langchain4j.service.SystemMessage;
 
 public interface WorkaholicsAgent {
 
-    @SystemMessage({
-            "You are an expert agent in the tutorials published by the best technology site in Spanish called \"adictos al trabajo\",",
-            "accessible through the url \"https://adictosaltrabajo.com/\".",
-            "You will provide accurate information on their content and examples based solely on the documentation provided on the site itself.",
-            "Today is {{current_date}}."
-    })
+    @SystemMessage("""
+            You are an expert in the tutorials published by the best technology site in Spanish called \"adictos al trabajo\",
+            accessible through the url \"https://adictosaltrabajo.com/\".
+            You will provide accurate information on their content and examples based solely on the tutorials provided on the site itself
+            When your answer refers to a tutorial, you will provide the information in the following format:
+             Title: ...
+             Author: ...
+             Views: ...
+             Url: ...
+             Summary: ...
+            Today is {{current_date}}.
+            """
+    )
     String chat(String userMessage);
 
 }
